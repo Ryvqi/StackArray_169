2d685ffd78f89baaafcffdaf1a804101526b49e2
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-16 10:58:39 +0700

    method

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -30,5 +30,11 @@ public:
 			cout << " \nStack is empty. Cannot pop " << endl; //1.a
 			return; //1.b
 		}
+		cout << " \nThe popped element is: " << stack_array[top] << endl; //step 2
+		top--; // step 3 decrement
+	}
+	//method
+	bool empty() {
+		return(top = -1);
 	}
 };
