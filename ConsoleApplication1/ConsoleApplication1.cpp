fb036c431a0f28c2e489e78cdb3a4ab978ea211e
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-16 11:21:35 +0700

    default

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -78,6 +78,14 @@ int main() {
 			}
 			s.pop();
 			break;
+		case '3': 
+			s.display();
+			break;
+		case '4':
+			return 0;
+		default:
+			cout << "\nInvalid Choice" << endl;
+			break;
 		}
 		}
 	}
