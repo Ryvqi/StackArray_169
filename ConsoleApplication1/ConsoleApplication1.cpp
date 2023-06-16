3c2231a404afa8c62c2dd07c49f1f23bc7cc0b67
Ryvqi <rifqikhabibi03@gmail.com>
2023-06-16 11:00:26 +0700

    mengisi kekosongan

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -33,8 +33,9 @@ public:
 		cout << " \nThe popped element is: " << stack_array[top] << endl; //step 2
 		top--; // step 3 decrement
 	}
-	//method
+	//method for check data is empty
 	bool empty() {
 		return(top = -1);
 	}
+	
 };
